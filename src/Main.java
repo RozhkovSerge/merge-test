d9631b3f579e0828a74dc321e71a3fa703674008
Serge Rozkov <rozkov_ss@mail.ru>
2023-06-02 10:57:24 +0300

    dev commit1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hello from dev");
         System.out.println("hello from dev1");
+        System.out.println("hello from dev2");
 
     }
 }
